027ca35c68302b2481f5f7010ab9c21adcc4957a
Fundora <0269746@students.dadeschools.net>
2019-04-16 12:07:29 -0400

    started something

---
+++ main.js
@@ -0,0 +1,22 @@
+var panel = $("#quiz-area");
+
+var countStartNumber = 30;
+
+//Question Set
+var questions = [
+
+    {
+        question: "Mark Zuckerberg is the founding director of which site?",
+        answers:["FackBook","Twitter","Instagram", "Myspace"],
+        correctAnswer: "FaceBook",
+        image: "Assets\Facebook gif.gif",
+
+    }
+    {
+        question: "What’s the coloured part of the human eye called?",
+        answers:["Pupil","Cornea","Iris", "Sclera"],
+        correctAnswer: "Iris",
+        image: "Assets\Eye gif.gif",
+
+    }
+]
