e14aa7615b23f32b66a1f35b5cff4ac3cdb43792
Fundora <0269746@students.dadeschools.net>
2019-04-18 11:44:21 -0400

    still need to finish

--- main.js
+++ main.js
@@ -11,12 +11,39 @@ var questions = [
         correctAnswer: "FaceBook",
         image: "Assets\Facebook gif.gif",
 
-    }
+    },
     {
         question: "What’s the coloured part of the human eye called?",
         answers:["Pupil","Cornea","Iris", "Sclera"],
         correctAnswer: "Iris",
         image: "Assets\Eye gif.gif",
 
-    }
+    },
 ]
+
+//variable to hold out setInterval
+var timer;
+
+var game = {
+
+    questions: questions,
+    currentQuestion: 0,
+    counter: countStartNumber,
+    correct: 0,
+    incorrect: 0,
+
+    countdown: function(){
+        game.counter--;
+        $("#counter-number").html(game.counter);
+        if (game.counter === 0){
+            console.log("TIMES UPPP");
+
+            game.timeUp();
+        }
+    },
+
+    loadQuestion: function(){
+        timer = setInterval
+    }
+
+}
